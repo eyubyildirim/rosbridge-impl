47d724205a4ae4e12aecc98f91294977f67049b6
eyub <eyub2001@gmail.com>
2020-11-14 16:17:34 +0300

    modified send_message so that it now takes two arguments; first for linearX of twist and second for angularZ of twist.

--- ros_hello.py
+++ ros_hello.py
@@ -1,35 +1,28 @@
-from geometry_msgs.msg import Twist, Vector3
 import time, roslibpy, threading
-import geometry_msgs.msg
 
 global client
 client = roslibpy.Ros(host='localhost', port=9090)
 client.run()
 
 
-def send_message():
+def send_message(linearX, angularZ):
     global client
-    count = 1
 
     talker = roslibpy.Topic(client, '/turtle1/cmd_vel', 'geometry_msgs/Twist')
 
-    time.sleep(2)
-
-    while client.is_connected:
-        twist = roslibpy.Message({
-            'linear': {
-                'x': 1.0,
-                'y': 0.0,
-                'z': 0.0
-            },
-            'angular': {
-                'x': 0.0,
-                'y': 0.0,
-                'z': 1.0
-            },
-        })
-        talker.publish(twist)
-        time.sleep(1)
+    twist = roslibpy.Message({
+        'linear': {
+            'x': linearX,
+            'y': 0.0,
+            'z': 0.0
+        },
+        'angular': {
+            'x': 0.0,
+            'y': 0.0,
+            'z': angularZ
+        },
+    })
+    talker.publish(twist)
 
     talker.unadvertise()
 
@@ -47,8 +40,7 @@ def receive_message():
         client.terminate()
 
 
-t1 = threading.Thread(target=send_message, args=())
 t2 = threading.Thread(target=receive_message, args=())
 
 t2.start()
-t1.start()
+
